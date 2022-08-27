bd23eda4b80f1ee5d6c2b9b1504dd88083f40f10
Madison Cagle <78162094+madisoncagle@users.noreply.github.com>
2022-08-27 13:05:11 -0500

    Start ispowerful.c
    (pretend like I know what I'm doing)

--- ispowerful.c
+++ ispowerful.c
@@ -1,7 +1,15 @@
 #include <stdio.h>
 
 // TODO
-int main()
+int main(int argc, char *argv[])
 {
+    // find every factor between 1 and number
+    // then find every prime number out of those
+
+    // loop: square and divide by each
+        // if division doesn't yield integer, print false and return 0
+    
+    // printf("true");
+
     return 0;
 }
