04e8c5b1b8edccd3b07bd79f511744075f8997c5
Madison Cagle <78162094+madisoncagle@users.noreply.github.com>
2022-08-29 09:59:47 -0500

    Add to ispowerful.c
    If this was C#, it would be a different story.

--- ispowerful.c
+++ ispowerful.c
@@ -1,14 +1,26 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 // TODO
 int main(int argc, char *argv[])
 {
-    // find every factor between 1 and number
-    // then find every prime number out of those
+    // extract integer from command line, stack overflow solution
+    char *a = argv[1];
+    int num = atoi(a);
 
-    // loop: square and divide by each
-        // if division doesn't yield integer, print false and return 0
-    
+    // loop through every integer between 1 and given number
+    // if factor, check if prime
+    // if prime, square and modulo
+    // if modulo != 0, print false and return 0
+
+    for (int i = 0; i < num; i++)
+    {
+        if (num % i == 0) // is a factor
+        {
+            // check if prime
+        }
+    }
+
     // printf("true");
 
     return 0;
