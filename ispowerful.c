3bb486903ee9583cf01a39fbb15b2c01bf1d8096
Madison Cagle <78162094+madisoncagle@users.noreply.github.com>
2022-08-29 14:05:55 -0500

    Delete some stuff
    Time to break out the rubber duck

--- ispowerful.c
+++ ispowerful.c
@@ -1,27 +1,30 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
 
-// TODO
+// bool isprime(int num)
+// {
+//     if (num < 2)
+//     {
+//         return false;
+//     }
+
+//     for (int i = 2; i < num; i++)
+//     {
+//         if (num % i == 0)
+//         {
+//             return false;
+//         }
+//     }
+
+//     return true;
+// }
+
 int main(int argc, char *argv[])
 {
     // extract integer from command line, stack overflow solution
     char *a = argv[1];
     int num = atoi(a);
-
-    // loop through every integer between 1 and given number
-    // if factor, check if prime
-    // if prime, square and modulo
-    // if modulo != 0, print false and return 0
-
-    for (int i = 0; i < num; i++)
-    {
-        if (num % i == 0) // is a factor
-        {
-            // check if prime
-        }
-    }
-
-    // printf("true");
 
     return 0;
 }
